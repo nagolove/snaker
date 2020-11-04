d331dd8654e6451a69c9dbc0543f859bf910fc86
nagolove <dekar.own@gmail.com>
2020-11-05 00:46:44 +0300

    Initial for unity 2019.4.13f1

---
+++ Assets/Snake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    GameObject head;
+    Vector2 ds;
+    public float speed = 10;
+    void Start()
+    {
+        head = GameObject.Find("head");
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("left"))
+            ds = new Vector2(-speed * Time.deltaTime, 0);
+        if (Input.GetKey("right"))
+            ds = new Vector2(speed * Time.deltaTime, 0);
+        if (Input.GetKey("up"))
+            ds = new Vector2(0, speed * Time.deltaTime);
+        if (Input.GetKey("down"))
+            ds = new Vector2(0, -speed * Time.deltaTime);
+
+        head.transform.position += new Vector3(ds.x, ds.y, 0);
+    }
+}
