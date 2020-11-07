7c644d712ce22eef3cbb7c61e8907c8bf418c421
nagolove <dekar.own@gmail.com>
2020-11-07 17:28:06 +0300

    new files.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Camera mainCamera;
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("z"))
+            mainCamera.orthographicSize *= 0.9f;
+        if (Input.GetKey("x"))
+            mainCamera.orthographicSize *= 1.1f;
+    }
+}
