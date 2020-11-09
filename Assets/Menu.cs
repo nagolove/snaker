12f1499a22656c817594abb5232613e7a3af2462
nagolove <dekar.own@gmail.com>
2020-11-09 19:55:51 +0300

    New scenes. Menu.

---
+++ Assets/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene[] scenes = SceneManager.GetAllScenes();
+        foreach(Scene scn in scenes)
+        {
+            Debug.Log(string.Format("scene {0}", scn.name));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
